380526b1f295df2043685f27543aa7cacaa3e25b
Mr. Root <root@surt.cs.umt.edu>
2018-07-02 14:25:51 -0600

    Updated runProfiles.py

--- runProfiles.py
+++ runProfiles.py
@@ -5,15 +5,13 @@ import h5py
 import time 
 from matplotlib.path import Path
 from helperFiles.classes.FlowIntegrator import *
-import matplotlib.pyplot as plt
 import math
-
+from numpy import sqrt
 def main(argv):
     allDataFile = h5py.File('AllDataSets0.h5', 'r')
-    smallerDataFile = h5py.File('GreenlandInBedCoord_V2.h5', 'r')
+    smallDataFile = h5py.File('GreenlandInBedCoord_V2.h5', 'r')
     os.chdir(argv[1])
     myFiles = []
-
     
     for file in glob.glob("*.h5"):
         myFiles.append(file)
@@ -31,16 +29,15 @@ def main(argv):
     print "starting thickness"
     t0 = time.time()
     datasetDict['thickness'] = Dataset('thickness', allDataFile)
+     
     print "thickness",  time.time() - t0
     t0 = time.time()
-    
-    smallerDataFile.close()
-    
     integrator = createIntegrator(datasetDict['VX'], datasetDict['VY'])
     
     
     for file in myFiles:
 
+	print "Starting file: ", file
         tempFile = h5py.File(file, 'r')
         shear1X = tempFile['Shear_0_x'][:]
         shear1Y = tempFile['Shear_0_y'][:]
@@ -52,61 +49,72 @@ def main(argv):
         
         
         tempFile.close()
-        numTrials = range(1,3)
+        numTrials = range(1,20)
         widthList = getWidths(myShears)
         
         method1TotalValues = []
         method2TotalValues = []
         method1StepValues = []
         method2StepValues = []
-        
+        method1TotalFluxA = []
+	method1StepFluxA = []
+	method2TotalFluxA = []
+	method2StepFluxA = [] 
         for x in numTrials:
 
-            method1TotalValue, method1StepValue, method2TotalValue, method2StepValue = 0,0,0,0
             
             try:
-                method1TotalValue, method1StepValue = method1(myShears,x,widthList, 1000, datasetDict, integrator)
+                method1TotalValue, method1StepValue, method1TotalFlux, method1StepFlux = method1(myShears,x,widthList, 1000, datasetDict, integrator)
             except:
                 e = sys.exc_info()[0]
                 print "error on method1 file: ", file, "\nerror trial: ", x, "\nerror: ", e
             method1TotalValues.append(method1TotalValue)
             method1StepValues.append(method1StepValue)
-            
+            method1TotalFluxA.append(method1TotalFlux)
+	    method1StepFluxA.append(method1StepFlux)
             try:
-                method2TotalValue, method2StepValue = method2(myShears,x,widthList, 1000, datasetDict)
+                method2TotalValue, method2StepValue, method2TotalFlux, method2StepFlux = method2(myShears,x,widthList, 1000, datasetDict)
             except:
                 e = sys.exc_info()[0]
                 print "error on method2 file: ", file, "\nerror trial: ", x, "\nerror: ", e
             method2TotalValues.append(method2TotalValue)
             method2StepValues.append(method2StepValue)
-            
+            method2TotalFluxA.append(method2TotalFlux)
+	    method2StepFluxA.append(method2StepFlux)
 
         try:
-            method3Value = method3(myShears, allDataFile)
+            method3Value, method3Flux = method3(myShears, allDataFile)
         except:
             e = sys.exc_info()[0]
             print "error on method3 file: ", file,"\nerror: ", e
         
         
-        outFileName = "widthAverage" + file
+        outFileName = "widthFile/widthAverage" + file
         outDataFile = h5py.File(outFileName, 'w')
         
         outDataFile.create_dataset("method1TotalVolume", data=method1TotalValues)
         outDataFile.create_dataset("method1StepVolume", data=method1StepValues)
+	outDataFile.create_dataset("method1TotalFlux", data=method1TotalFluxA)
+	outDataFile.create_dataset("method1StepFlux", data=method1StepFluxA)
         outDataFile.create_dataset("method2TotalVolume", data=method2TotalValues)
         outDataFile.create_dataset("method2StepVolume", data=method2StepValues)
-        
-        
+	outDataFile.create_dataset("method2TotalFlux", data=method2TotalFluxA)
+	outDataFile.create_dataset("method2StepFlux", data=method2StepFluxA)
+	 
         method3ValueArray = []
         for i in range(0, len(method1TotalValues)):
             method3ValueArray.append(method3Value)
-        outDataFile.create_dataset("method3Value", data=method3ValueArray)
+	
+        outDataFile.create_dataset("method3Volume", data=method3ValueArray)
+	method3ValueArray = []
+	for i in range(0, len(method1TotalValues)):
+	    method3ValueArray.append(method3Flux)
+        outDataFile.create_dataset("method3Flux", data=method3ValueArray)
         
         
-        
-        outDataFile.close()
-        
+        outDataFile.close()        
     
+    print "totalTime",  time.time() - t0
 def getWidths(shears):
     shear1 = np.asarray(shears[0])
     shear2 = np.asarray(shears[1])
@@ -147,17 +155,26 @@ def method1(shears, numSamples, widthList, resolution,datasetDict, integrator):
     print "Ommitted ", len(flowlineSamples) - numSamples, " lines. Out of a possible ", numSamples
     
     thicknessList = []
+    velocityList = []
     for step in range(len(flowlineSamples[0])):
         tempThickness = []
+	tempVelocity = []
+
         for sample in range(len(flowlineSamples)):
             samplePoint = flowlineSamples[sample][step]
             tempThickness.append(datasetDict['thickness'].getInterpolatedValue(samplePoint[0], samplePoint[1]))
+            tempVelocity.append(sqrt((datasetDict['VX'].getInterpolatedValue(samplePoint[0], samplePoint[1]))**2 + (datasetDict['VY'].getInterpolatedValue(samplePoint[0], samplePoint[1]))**2))
         thicknessList.append(np.mean(np.asarray(tempThickness)))
+	velocityList.append(np.mean(np.asarray(tempVelocity)))
     thicknessList = np.asarray(thicknessList)
-    
+    velocityList = np.asarray(velocityList)
+
+
+    flux = (velocityList[:] * widthList[:]) * resolution    
     volume = (thicknessList[:] * widthList[:]) * resolution
+
     
-    return sum(volume), volume
+    return sum(volume), volume, sum(flux), flux
         
         
         
@@ -170,9 +187,11 @@ def method2(shears, numSamples, widthList, resolution, datasetDict):
     shear2 = np.asarray(shears[1])
     
     avgThicknessList = []
-    
+    avgVelocityList = []    
+
     for x in range(0, len(shear1)):
         totalThickness = 0
+	totalVelocity = 0
         shearPoints = [[shear1[x][0], shear1[x][1]], [shear2[x][0], shear2[x][1]]]
         
         dx = (shearPoints[1][0] - shearPoints[0][0]) / (numSamples + 1)
@@ -185,12 +204,14 @@ def method2(shears, numSamples, widthList, resolution, datasetDict):
             currPoint[1] = currPoint[1] + dy
             
             totalThickness = totalThickness + datasetDict['thickness'].getInterpolatedValue(currPoint[0], currPoint[1])
-            
+            totalVelocity = totalVelocity + (sqrt((datasetDict['VX'].getInterpolatedValue(currPoint[0], currPoint[1]))**2 + (datasetDict['VY'].getInterpolatedValue(currPoint[0], currPoint[1]))**2))
         avgThicknessList.append(totalThickness/numSamples)
+	avgVelocityList.append(totalVelocity/numSamples)
     
     volume = ((avgThicknessList * widthList) * resolution)
+    flux = ((avgVelocityList * widthList) * resolution)
     
-    return sum(volume), volume
+    return sum(volume), volume, sum(flux), flux
         
 def method3(shears, allDataFile):
 
@@ -218,7 +239,9 @@ def method3(shears, allDataFile):
     ys = allDataFile['y'][:]
 
     thickness = allDataFile['thickness'][:]
-    
+    vx = allDataFile['VX'][:]
+    vy = allDataFile['VY'][:]
+
     allDataFile.close()
 
     xx, yy = np.meshgrid(xs, ys)
@@ -226,8 +249,8 @@ def method3(shears, allDataFile):
     XYFlat = XY.reshape(-1, 2)
 
     thicknessFlat = thickness.reshape(-1)
-
-
+    vxFlat = vx.reshape(-1)
+    vyFlat = vy.reshape(-1)
 
 
 
@@ -241,17 +264,24 @@ def method3(shears, allDataFile):
     numX = 10018
     samplePoints = XYFlat[int(math.floor(YToCutMax)*numX):-(int(math.floor(YToCutMin) * numX))]
     thicknessFlat = thicknessFlat[int(math.floor(YToCutMax)*numX):-(int(math.floor(YToCutMin) * numX))]
-
+    vxFlat = vxFlat[int(math.floor(YToCutMax)*numX):-(int(math.floor(YToCutMin) * numX))]
+    vyFlat = vyFlat[int(math.floor(YToCutMax)*numX):-(int(math.floor(YToCutMin) * numX))]
 
 
     newSamplePoints = []
     newThickness = []
+    newVX = []
+    newVY = []
     for i in range(numX):
         newSamplePoints.extend(samplePoints[i::numX])
         newThickness.extend(thicknessFlat[i::numX])
+        newVX.extend(vxFlat[i::numX])
+        newVY.extend(vyFlat[i::numX])
 
     newSamplePoints = np.asarray(newSamplePoints)
     newThickness = np.asarray(newThickness)
+    newVX = np.asarray(newVX)
+    newVY = np.asarray(newVY)
 
 
 
@@ -279,12 +309,13 @@ def method3(shears, allDataFile):
 
     finalSamplePoints = newSamplePoints[XToCutMin*numY:-(XToCutMax*numY)]
     finalThicknessPoints = newThickness[XToCutMin*numY:-(XToCutMax*numY)]
-
+    finalVX = newVX[XToCutMin*numY:-(XToCutMax*numY)]
+    finalVY = newVY[XToCutMin*numY:-(XToCutMax*numY)]
 
 
 
     thicknessValuesInShape = []
-
+    velocityValuesInShape = []
 
 
     pointsInShape = []
@@ -296,12 +327,13 @@ def method3(shears, allDataFile):
 
         if myPath.contains_point(point):
             thicknessValuesInShape.append(finalThicknessPoints[i])
+	    velocityValuesInShape.append(sqrt(finalVX[i]**2 + finalVY[i]**2))
             pointsInShape.append(point)
 
 
     trueVolume = sum(thicknessValuesInShape) * 150**2
-
-    return trueVolume
+    trueFlux = sum(velocityValuesInShape) *150**2
+    return trueVolume, trueFlux
 
 #pulled from software and modified. 
 class Dataset:
@@ -320,8 +352,8 @@ class Dataset:
         map['proj_y1'] = dataFile['y'][:][-1]
        
 
-        bed_xarray = linspace(map['proj_x0'], map['proj_x1'], map['x1'], endpoint=True)
-        bed_yarray = linspace(map['proj_y1'], map['proj_y0'], map['y1'], endpoint=True)
+        bed_xarray = np.linspace(map['proj_x0'], map['proj_x1'], map['x1'], endpoint=True)
+        bed_yarray = np.linspace(map['proj_y1'], map['proj_y0'], map['y1'], endpoint=True)
 
         if self.name == 'velocity':
             self.data, self.vx, self.vy = self.setData(name)
